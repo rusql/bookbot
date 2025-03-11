ca6aa6de5412253b635cf5e9ecb84f2fc1e0f145
rusql <russell.subscriptions@gmail.com>
2025-03-11 16:12:25 +0200

    update to allow books to be specified in the command line

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
+from stats import get_word_count
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    print_report(book_path, book_text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
+    print_report(book_text)
 
 
 def sort_on(item):
     return item[1]
 
 
-def print_report(book_path, book_text):
+def print_report(book_text):
     word_count = get_word_count(book_text)
     character_list = list(get_character_map(book_text).items())
     character_list.sort(key=sort_on, reverse=True)
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in document")
+    print(f"--- Begin report of {sys.argv[1]} ---")
+    print(f"{word_count} words found in the document")
     print()
     for c in character_list:
         if c[0].isalpha():
-            print(f"The {c[0]} character was found {c[1]} times")
+            print(f"{c[0]}: {c[1]}")
     print("--- End report ---")
 
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
-
-
-def get_word_count(s):
-    return len(s.split())
 
 
 def get_character_map(s):
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_word_count(s):
+    return len(s.split())
